5bdb484d0b8798bc158c399f44704da680ae1bb8
Yaroslav <yaroslav1992russia@gmail.com>
2021-03-21 07:02:12 +0200

    added products of the week  and some fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-$(function () {});
+$(function () {
+  $(".slider__list").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  var mixer = mixitup(".products__cards");
+});
