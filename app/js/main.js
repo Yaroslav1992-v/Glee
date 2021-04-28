2114ceef279a7ad1292bdf168a0684349a115036
Yaroslav <yaroslav1992russia@gmail.com>
2021-04-28 17:52:16 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
 $(function () {
+  $(".product-details__link").on("click", function (e) {
+    e.preventDefault();
+    $(".product-details__link").removeClass("product-details__link--active");
+    $(this).addClass("product-details__link--active");
+    $(".product-details__tab").removeClass("product-details__tab--active");
+    $($(this).attr("href")).addClass("product-details__tab--active");
+  });
   if ($(".slider-main__list").length > 0) {
     $(".slider-main__list").slick({
       dots: true,
@@ -6,6 +13,69 @@ $(function () {
       fade: true,
       autoplay: true,
       autoplaySpeed: 2000,
+    });
+  }
+  if ($(".products-slider__list").length > 0) {
+    $(".products-slider__list").slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      prevArrow:
+        '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="9" viewBox="0 0 22 9"><defs><style>.cls-1 {fill: #a3bbc8;fill-rule: evenodd;}</style></defs><path class="cls-1" d="M1460.7,1330.53h-16.94l0.1-2.86a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-0.18-.17a0.267,0.267,0,0,0-.4,0l-4.2,4.21a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l4.2,4.21a0.29,0.29,0,0,0,.4,0l0.18-.17a0.323,0.323,0,0,0,.08-0.21,0.3,0.3,0,0,0-.08-0.2l-0.12-2.86h16.97a0.3,0.3,0,0,0,.29-0.3v-1.37A0.291,0.291,0,0,0,1460.7,1330.53Z" transform="translate(-1439 -1327)"/></svg></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="9" viewBox="0 0 22 9"><defs> <style>.cls-1 {fill: #a3bbc8;fill-rule: evenodd; } </style></defs><path class="cls-1" d="M1509.3,1332.47h16.94l-0.1,2.86a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l0.18,0.17a0.267,0.267,0,0,0,.4,0l4.2-4.21a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-4.2-4.21a0.29,0.29,0,0,0-.4,0l-0.18.17a0.323,0.323,0,0,0-.08.21,0.318,0.318,0,0,0,.08.2l0.12,2.86h-16.97a0.3,0.3,0,0,0-.29.3v1.37A0.291,0.291,0,0,0,1509.3,1332.47Z" transform="translate(-1509 -1327)"/></svg></button>',
+      responsive: [
+        {
+          breakpoint: 1220,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 980,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+
+        {
+          breakpoint: 548,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  }
+
+  if ($(".photo-slide__min").length > 0) {
+    $(".photo-slide__min").slick({
+      asNavFor: ".photo-slide__big",
+      focusOnSelect: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      vertical: true,
+      draggable: false,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 980,
+          settings: {
+            vertical: false,
+          },
+        },
+      ],
+    });
+    $(".photo-slide__big").slick({
+      asNavFor: ".photo-slide__min",
+      draggable: false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true,
     });
   }
   if ($(".slider-brands__list").length > 0) {
@@ -106,7 +176,7 @@ $(function () {
       $(".price__to").text(data.to + ".00");
     },
   });
-  $(".select-style").styler();
+  $(".select-style,.product-details__input").styler();
   $(".resent__star").rateYo({
     starWidth: "12px",
     starHeight: "12px",
@@ -116,7 +186,7 @@ $(function () {
     starSvg:
       '<svg  viewBox="0 0 576 512"><path  d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>',
   });
-  $(".product-card__star").rateYo({
+  $(".product-card__star,.product-details__star").rateYo({
     starWidth: "19px",
     starHeight: "19px",
     normalFill: "#d6d6d6",
